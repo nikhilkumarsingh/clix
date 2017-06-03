15a972b014017ca8c77680bf4222ae68edd8e3ca
Dibya Das <dibyadascool@gmail.com>
2017-06-03 18:27:18 +0530

    added support for setting alternate key bindings

--- clix/clix.py
+++ clix/clix.py
@@ -1,7 +1,9 @@
 import json
 import xerox
-from .pyxhook import HookManager
-from .gui import clipboard
+from pyxhook import HookManager
+from gui import clipboard
+import argparse
+
 
 # clipboard
 clips = []
@@ -9,23 +11,39 @@ clips = []
 active = 0
 # previously logged key
 prev_Key = None
+available_keys = {	'LCTRL' : 'Control_L',
+					'RCTRL' : 'Control_R',
+					'ALT'   : "",
+					'LSHIFT' : 'Shift_L',
+					'RSHIFT' : 'Shift_R',
+					'SPACE' : 'space'
+					}
+
+key_binding = ['Control_L','space']
 
 def OnKeyPress(event):
-	global prev_Key, active
-	if event.Key == 'space' and prev_Key == 'Control_L' and active == 0:
+	global prev_Key, active,key_binding
+	if event.Key == key_binding[1] and prev_Key == key_binding[0] and active == 0:
 		active = 1
 		clipboard(clips)
 		active = 0
 		prev_Key = None
 	elif event.Key == 'c' and prev_Key == 'Control_L':
 		text = xerox.paste(xsel = True)
-		clips.append(text)
+		clips.append(text) 
 		print("You just copied: {}".format(text))
 	else:
 		prev_Key = event.Key
 
 def main():
-	new_hook = HookManager()
+	global key_binding
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s","--set-keybinding",type=str,default="LCTRL+SPACE",help="Set alternate key binding. Default is LCTRL+SPACE")
+	args = parser.parse_args()
+	args_dict = vars(args)
+	if args_dict['set_keybinding'] is not "LCTRL+SPACE":
+		key_binding = [available_keys[args_dict['set_keybinding'].split('+')[0]],available_keys[args_dict['set_keybinding'].split('+')[1]]]
+	new_hook = HookManager()	
 	new_hook.KeyDown = OnKeyPress
 	new_hook.HookKeyboard()
 	new_hook.start()
