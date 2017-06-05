19c894615edd185762cfd124814e94515b7fd63b
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-06-05 16:14:58 +0530

    improved indentation

--- clix/utils.py
+++ clix/utils.py
@@ -1,8 +1,13 @@
-available_keys = {	'LCTRL' : 'Control_L',
-					'RCTRL' : 'Control_R',
-					'LALT'   : "Alt_L",
-					'RALT'   : "Alt_R",
-					'LSHIFT' : 'Shift_L',
-					'RSHIFT' : 'Shift_R',
-					'SPACE' : 'space'
-					}
+"""
+These keys have been assigned values specific to pyxhook.
+Make required changes when support for pyHook is implemented.
+"""
+available_keys = {	
+	'LCTRL' : 'Control_L',
+	'RCTRL' : 'Control_R',
+	'LALT'   : "Alt_L",
+	'RALT'   : "Alt_R",
+	'LSHIFT' : 'Shift_L',
+	'RSHIFT' : 'Shift_R',
+	'SPACE' : 'space'
+}
