dc7bdce43bd2d14b1c327849c848b77330cd3026
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-06-05 16:14:19 +0530

    improved formatting

--- clix/clix.py
+++ clix/clix.py
@@ -1,11 +1,11 @@
+import os, sys
 import json
 import xerox
+import pickle
+import argparse
 from .pyxhook import HookManager
 from .gui import clipboard
-import argparse
 from .utils import available_keys
-import sys
-import pickle
 
 # clipboard
 clips = []
@@ -13,69 +13,97 @@ clips = []
 active = 0
 # previously logged key
 prev_Key = None
+# path to site package
+curr_dir = os.getcwd()
 
-with open("clix/config", "rb") as f:
-    key_binding = pickle.load(f)
+# loading key_binding from config file 
+with open(curr_dir + "/clix/config", "rb") as f:
+        key_binding = pickle.load(f)
 
 
 def OnKeyPress(event):
+    """
+    function called when any key is pressed
+    """
     global prev_Key, active, key_binding
+
     if event.Key == key_binding[1] and prev_Key == key_binding[0] and active == 0:
         active = 1
         clipboard(clips)
         active = 0
         prev_Key = None
+
     elif event.Key == 'c' and prev_Key == 'Control_L':
         text = xerox.paste(xsel=True)
         clips.append(text)
         print("You just copied: {}".format(text))
+
     else:
         prev_Key = event.Key
 
 
 def _show_available_keybindings():
-    print("Available Keys:- "+"\n")
-    for i in available_keys:
-        print(i)
+    """
+    function to show available keys
+    """
+    print("Available Keys: "+"\n")
+    for key in available_keys:
+        print(key)
 
 
-def _show_current_keybinding():
-    with open("clix/config", "rb") as f:
-        key_binding = pickle.load(f)
-        temp = {b: a for a, b in available_keys.items()}
-        print(temp[key_binding[0]] + "+" + temp[key_binding[1]])
+def get_current_keybinding():
+    """
+    function to show current key-binding
+    """
+    global key_binding
+    temp = {b: a for a, b in available_keys.items()}
+    return temp[key_binding[0]] + "+" + temp[key_binding[1]]
 
 
 def main():
+    """
+    main function (CLI endpoint)
+    """
     global key_binding
+
     parser = argparse.ArgumentParser()
+    
     help = """Set alternate key binding. Default is LCTRL+SPACE
-                Format :- <KEY1>+<KEY2>. Ex:- RCTRL+RALT
+                Format :- <KEY1>+<KEY2>. Ex:- RCTRL+RALT .
                 To see availble key bindings use 'clix -a' option"""
-    parser.add_argument("-s", "--set-keybinding", type=str,
-                        default="LCTRL+SPACE", help=help)
-    parser.add_argument("-a", "--available-keybindings",
-                        help="Show available key bindings", action="store_true")
-    parser.add_argument("-c", "--show-current-keybinding", action="store_true")
+    
+    parser.add_argument("-s", "--set-keybinding", type = str,
+                        default = None, help = help)
+
+    parser.add_argument("-a", "--show-available-keybindings",
+                        help = "Show available key bindings", action = "store_true")
+    
+    parser.add_argument("-c", "--show-current-keybinding", action = "store_true")
+    
     args = parser.parse_args()
     args_dict = vars(args)
-    if args.show_current_keybinding is True:
-        _show_current_keybinding()
+    
+    if args.show_current_keybinding:
+        print("Current key binding is: {}".format(get_current_keybinding()))
         sys.exit()
-    if args.available_keybindings is True:
+
+    elif args.show_available_keybindings:
         _show_available_keybindings()
         sys.exit()
-    if args_dict['set_keybinding'] is not "LCTRL+SPACE":
+    
+    elif args.set_keybinding:
         try:
-            key_binding = [available_keys[args_dict['set_keybinding'].split(
-                '+')[0]], available_keys[args_dict['set_keybinding'].split('+')[1]]]
+            keys = args_dict['set_keybinding'].split('+')
+            key_binding = [available_keys[keys[0]], available_keys[keys[1]]]
         except KeyError:
             print("Please follow the correct format.")
+        else:
+            with open(curr_dir + "/clix/config", "wb") as f:
+                pickle.dump(key_binding, f, protocol = 2)
         finally:
-            with open("clix/config", "wb") as f:
-                pickle.dump(key_binding, f, protocol=2)
             sys.exit()
 
+    # start key-logging session
     new_hook = HookManager()
     new_hook.KeyDown = OnKeyPress
     new_hook.HookKeyboard()
